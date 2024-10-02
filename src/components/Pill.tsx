d619febf0c43fa40cd17a2be2840d958895099c3
Anuj0609 <anujshrivastava982@gmail.com>
2024-10-02 10:36:59 +0530

    improve spacing in experience section

--- src/components/Pill.tsx
+++ src/components/Pill.tsx
@@ -1,6 +1,6 @@
 export const Pill = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="border-2 border-gray-800 px-6 py-1 rounded-2xl text-center">
+    <div className="border-2 border-gray-800 px-6 py-1 rounded-2xl text-center md:mt-4">
       {children}
     </div>
   );
